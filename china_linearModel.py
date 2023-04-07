791185f64730434c1668e0d8ac17f79cb05ac0d5
Sebastian Ibanez <Sibanez0081@conestogac.on.ca>
2023-04-07 13:22:01 -0400

    Finished predicting the linear model.

--- china_linearModel.py
+++ china_linearModel.py
@@ -3,26 +3,43 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-sns.set(style="white", color_codes=True)
-
 #Emissions
-data = pd.read_csv('em_gdp.csv', index_col=0)
-
-#Get only China's data
+data = pd.read_csv('em_gdp.csv')
 data = data[data['country'] == 'China']
-data = data[data['year'] <= 2020]
-data = data[['year','gdp','emissions']]
-#plt.plot(data['year'], data['emissions'], color='red')
 
+x = data['year']
+y = data['emissions']
+z = data['gdp']
 
-emissions = data[['emissions']]
-year = data[['year']]
+ychange = y.pct_change()
+ychange = ychange.fillna(0)
+ymean = ychange.mean()
 
-print(emissions)
+y_m, y_b = np.polyfit(x, y, 1)
+y_line = np.polyval([y_m, y_b], x)
+
+z_m, z_b = np.polyfit(x, z, 1)
+z_line = np.polyval([z_m, z_b], x)
+
+zchange = z.pct_change()
+zchange = zchange.fillna(0)
+zmean = zchange.mean()
 
 #Predict
-emissions = data[['emissions']].to_numpy()
-year = data[['year']].to_numpy()
-m, b = np.polyfit(emissions, year, 1)
-plt.plot(emissions, m * emissions + b, color='green')
+plt.subplot(2, 1, 1)
+plt.plot(x, y, color='#ff595e')
+plt.plot(x, y_line, color='#ffb703', linestyle='--')
+plt.title('China\'s Emissions - Linear Predicted Model')
+plt.legend(['Actual', 'Predicted'])
+plt.xlabel('Year')
+plt.ylabel('Emissions')
+
+plt.subplots_adjust(hspace=0.75)
+plt.subplot(2, 1, 2)
+plt.plot(x, z, color='#8ac926')
+plt.plot(x, z_line, color='#ffb703', linestyle='--')
+plt.title('China\'s GDP - Linear Predicted Model')
+plt.legend(['Actual', 'Predicted'])
+plt.xlabel('Year')
+plt.ylabel('GDP')
 plt.show()
