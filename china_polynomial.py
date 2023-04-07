3536866451e8d80fdd6da9adb3896abd94cb75eb
Sebastian Ibanez <Sibanez0081@conestogac.on.ca>
2023-04-07 17:03:28 -0400

    Finished China's graph.

---
+++ china_polynomial.py
@@ -0,0 +1,35 @@
+import math
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.metrics as metrics
+
+data = pd.read_csv('em_gdp.csv')
+data = data[data['country'] == 'China']
+
+x = data['year'].to_numpy()
+y = data['emissions'].to_numpy()
+z = data['gdp'].to_numpy()
+
+ymodel = np.poly1d(np.polyfit(x, y, 4))
+zmodel = np.poly1d(np.polyfit(x, z, 4))
+
+myline = np.linspace(1960, 2020, 100)
+
+plt.subplot(2, 1, 1)
+plt.plot(x, y, color='#ff595e')
+plt.plot(myline, ymodel(myline), color='#ffb703', linestyle='--')
+plt.title('China\'s Emissions - Exponential Predicted Model')
+plt.legend(['Actual', 'Predicted'])
+plt.xlabel('Year')
+plt.ylabel('Emissions')
+
+plt.subplots_adjust(hspace=0.75)
+plt.subplot(2, 1, 2)
+plt.plot(x, z, color='#8ac926')
+plt.plot(myline, zmodel(myline), color='#ffb703', linestyle='--')
+plt.title('China\'s GDP - Exponential Predicted Model')
+plt.legend(['Actual', 'Predicted'])
+plt.xlabel('Year')
+plt.ylabel('GDP')
+plt.show()
